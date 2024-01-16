907cb7f614a247db629a44fb6b73fdb100570b2f
haixin SUN <haixin.sun@terranet.se>
2024-01-16 16:36:54 +0100

    finish the dataloader

--- try.py
+++ try.py
@@ -6,6 +6,9 @@ df = pd.read_csv('data/daily_data_simple.csv')
 data = df[df.date == '2019-01-02'].reset_index(drop=True)
 data.set_index('index_code', inplace=True)
 flattened_data = data.T.stack().to_frame().T
-print(flattened_data.head())
+
+flattened_data = flattened_data.reset_index(drop=True)
+flattened_data.to_csv('data/flattened_data1.csv', index=False)
+# merge second row into column names
+flattened_data.columns = flattened_data.columns.map('/'.join)
 flattened_data.to_csv('data/flattened_data.csv', index=False)
-# df = pd.read_csv('data/flattened_data.csv')
